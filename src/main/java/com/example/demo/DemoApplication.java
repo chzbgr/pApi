72938259fd0aa8f79841911b76ae07dd746de002
jjunhyeon <sonjs01@naver.com>
2021-04-19 17:53:54 +0900

    junhyeon commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    // junhyeon test111111
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
