a61dec90ae52a12cd1b19121f5cf636499e20f0f
bkhun1214 <bkhun1214@gmail.com>
2021-04-19 18:01:50 +0900

    kihun commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//dfdsfsdfsadfasfasdf
 public class DemoApplication {
-//testsdfsdfsdfsdf
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
